4914ffd740ccd1cf01810e3ebc36802f67446e2b
drj613 <drj613@gmail.com>
2018-02-03 13:45:43 -0800

    Basic layout, a few bells, no whistles

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(window).on("load", function(){
+    var greeting = [ "<h1>Hello! </h1>", "<h1>I'm DJ.</h1>", "<h1>Welcome to my portfolio!</h1>", "<h1>Click below to learn more about me. :)</h1>" ];
+    $("#continue").hide();
+
+    new Typed('#insertion', {
+        strings: greeting,
+        typeSpeed: 100,
+        backSpeed: 50,
+        onComplete: function(self){$("#continue").fadeIn("slow");}
+    });
+
+    $("#continue").one("click", function(){
+        $(".container").removeClass("hidden");
+    });
+    
+    function toMainTop(){
+        $('html, body').animate({
+            scrollTop: $('.container').offset().top
+        }, 1000);
+    }
+
+    $("#continue").on("click", function(){
+        toMainTop();
+        // $('.container').animate({
+
+        // }, 1000);
+    });
+
+    $(".icon").animate
+
+
+});
