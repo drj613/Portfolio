d9798c1c433c370df9a017438965c4f1ac7bda31
drj613 <drj613@gmail.com>
2018-02-17 10:22:31 -0800

    creating constructors for icons to track state for animation

--- js/main.js
+++ js/main.js
@@ -28,7 +28,87 @@ $(window).on("load", function(){
         // }, 1000);
     });
 
-    
+    function hideThings(exceptThis){
+        let icons = ["#contact", "#about", "#tech", "#works", "#resume"];
+
+        for (let i=0; i < icons.length; i++){
+            if(icons[i]!= exceptThis){
+                $(icons[i]).hide();
+            }
+        }
+    }
+
+    function Icon(id, closeHTML, openHTML){
+        this.id = id;
+        this.closeHTML = closeHTML,
+        this.openHTML = openHTML,
+        this.state = "closed"
+
+        function toggleOpenClose(){
+            if (this.state === "closed"){
+                this.state = "opened";
+                $(id).removeClass("icon");
+                $(id).addClass("expanded");
+            } else {
+                $(id).removeClass("expanded");
+                $(id).addClass("icon");
+            }
+        }
+    }
+
+    // Constants for innards or expanded icons
+    const aboutMe = '<p>Hi!</p>';
+    const techUsed = '<p>Hi!</p>';
+    const worksDone = '<p>Hi!</p>';
+    const resume = '<p>Hi!</p>';
+
+    // Objects for tracking state of icons and their content
+    var aboutIcon = new Icon(
+        '#about',
+        '<i class="fa fa-user"></i>',
+        aboutMe);
+    var techIcon = new Icon(
+        '#tech',
+        '<i class="fa fa-wrench"></i>',
+        techUsed);
+    var worksIcon = new Icon(
+        '#works',
+        '<i class="fa fa-briefcase">',
+        worksDone);
+    var resumeIcon = new Icon(
+        '#resume',
+        '<span> <i class="fa fa-file-text"></i><i class="fa fa-sub fa-user"></i></span>',
+        resume);
+    var allIcons = [aboutIcon, techIcon, worksIcon, resumeIcon];
+
+    // Determine which icon was clicked on, then pass it into iconObject to manipulate
+    var iconObject = "";
+    $(".icon").on("click", function(){
+        let selectedIcon = this.id;
+
+        for (var i=0; i<allIcons.length, i++;){
+            if(selectedIcon === allIcons[i].id){
+                iconObject = allIcons[i];
+            }
+        }
+    });
+
+
+    // Testing animation for icons
+    // $("#contact").on("click", function(){
+    //     clickedId = this.id
+    //     console.log(clickedId);
+    //     hideThings("#" +this.id);
+    //     $("#contact").animate({
+    //         height: "100%",
+    //         width: "100%",
+    //         position: "absolute",
+    //         top: "0",
+    //         left: "0"
+    //     }, 1500, function(){
+    //         console.log("Boom!");
+    //     })
+    // });
 
 
 });
