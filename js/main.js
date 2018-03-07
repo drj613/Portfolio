d709537b5c395ca0c55e81bb010ad782038e140d
drj613 <drj613@gmail.com>
2018-03-06 16:47:35 -0800

    Finished about me, portfolio, resume, stack sections. Added section close on Esc

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 $(window).on("load", function(){
     var greeting = [ "<h1>Hello! </h1>", "<h1>I'm DJ.</h1>", "<h1>Welcome to my portfolio!</h1>", "<h1>Click below to learn more about me. :)</h1>" ];
     $("#continue").hide();
+    $(".expanded").hide();
+    $("#close").hide();
 
     new Typed('#insertion', {
         strings: greeting,
-        typeSpeed: 100,
-        backSpeed: 50,
+        // typeSpeed: 100,
+        // backSpeed: 50,
+        typeSpeed: 1,
+        backSpeed: 1,
         onComplete: function(self){
             $("#continue").toggle("bounce", {times: 5}, "slow");
         }
@@ -28,70 +32,47 @@ $(window).on("load", function(){
         // }, 1000);
     });
 
-    function hideThings(exceptThis){
-        let icons = ["#contact", "#about", "#tech", "#works", "#resume"];
+    let icons = ["#contact", "#about", "#tech", "#works", "#resume"];
 
-        for (let i=0; i < icons.length; i++){
-            if(icons[i]!= exceptThis){
-                $(icons[i]).hide();
-            }
+    function hideIcons(){
+        for (let i=0; i<icons.length; i++){
+            $(icons[i]).hide();
+        }
+    }
+    function showIcons(){
+        for (let i=0; i<icons.length; i++){
+            $(icons[i]).show();
         }
     }
 
-    function Icon(id, closeHTML, openHTML){
-        this.id = id;
-        this.closeHTML = closeHTML,
-        this.openHTML = openHTML,
-        this.state = "closed"
-
-        function toggleOpenClose(){
-            if (this.state === "closed"){
-                this.state = "opened";
-                $(id).removeClass("icon");
-                $(id).addClass("expanded");
-            } else {
-                $(id).removeClass("expanded");
-                $(id).addClass("icon");
-            }
-        }
-    }
-
-    // Constants for innards or expanded icons
-    const aboutMe = '<p>Hi!</p>';
-    const techUsed = '<p>Hi!</p>';
-    const worksDone = '<p>Hi!</p>';
-    const resume = '<p>Hi!</p>';
-
-    // Objects for tracking state of icons and their content
-    var aboutIcon = new Icon(
-        '#about',
-        '<i class="fa fa-user"></i>',
-        aboutMe);
-    var techIcon = new Icon(
-        '#tech',
-        '<i class="fa fa-wrench"></i>',
-        techUsed);
-    var worksIcon = new Icon(
-        '#works',
-        '<i class="fa fa-briefcase">',
-        worksDone);
-    var resumeIcon = new Icon(
-        '#resume',
-        '<span> <i class="fa fa-file-text"></i><i class="fa fa-sub fa-user"></i></span>',
-        resume);
-    var allIcons = [aboutIcon, techIcon, worksIcon, resumeIcon];
-
-    // Determine which icon was clicked on, then pass it into iconObject to manipulate
-    var iconObject = "";
+    // Determine which icon was clicked on
+    
     $(".icon").on("click", function(){
-        let selectedIcon = this.id;
-
-        for (var i=0; i<allIcons.length, i++;){
-            if(selectedIcon === allIcons[i].id){
-                iconObject = allIcons[i];
-            }
-        }
+        let toShow = "#" + "full" + this.id;
+        hideIcons();
+        $(toShow).toggle("puff");
+        $("#close").show();
     });
+
+    $(".expanded").on("click", function(){
+        showIcons();
+        $(".expanded").hide();
+        $("#close").hide();
+    });
+
+    $("#close").on("click", function(){
+        showIcons();
+        $(".expanded").hide();
+        $("#close").hide();
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) { 
+            showIcons();
+            $(".expanded").hide();
+            $("#close").hide();
+       }
+   });
 
 
     // Testing animation for icons
